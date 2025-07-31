7565708fa37e4cfdd22680eecc7729fcebdc657c
Mwikali <mwikalisharon749@gmail.com>
2025-07-31 09:42:26 +0300

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,38 @@
+#PLP = "Academy"
+# Student_name ="Mwikali Sharon"
+# Student_ID = 1001
+# Student_Age = 21
+# Fees_clearance = True
+
+# #output
+# print(Student_name,Student_ID,Student_Age,Fees_clearance)
+
+# #lists
+# Languages = ["Python","Java","C++","Javascript"]
+# print(languages)
+
+# Shopping_list = ["Milk", "Eggs", "Bread", "Butter" ]
+# print(shopping_list)
+
+# #Tuples - its immutable - u cannot change
+# Products = ('XBox',499.99,'Habibi',23)
+# print(products)
+
+#Sets
+# Set_A = {1,2,3,4,5}
+# Set_B = {4,5,6,7,8}
+# print(Set_A)
+# print(Set_B)
+
+#Dictionaries
+# Student_info = {'Name':'Mwikali Sharon','ID':'1001', 'Age':'20.2','Fees clearance':'True'}
+# print(student_info)
+# Capital_city = {'Kenya':'Nairobi', 'Nigeria':Lagos, 'South Africa':'Cape Town , 'Uganda':'Kampala'}
+
+#logical operators
+a = 10
+b = 20
+c = 30
+print(a < b and b > c)  #True #and
+print(a < b or b > c)#True #or
+print(not (a < b)) #false #not
